8f3184cf6533fc94a4190c7833d1bf1019d4e485
Konrad <konradhyzy@gmail.com>
2018-01-27 14:09:24 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function firstStart(){
 	if(gameStarted == false){
 		j = 0;
 		gameStarted = true;
-		for (var i = 0; i < 20; i++){
+		for (var l = 0; l < 20; l++){
 			moves.push(Math.floor(Math.random()*4));
 		}
 
@@ -35,12 +35,31 @@ var p = 0;
 //
 var playerTurn = false;
 
+//Hardcore mode is activated once player clicks on Strict BUTTON
+var hardcore = false;
+function hardcoreMode(){
+	if(hardcore == false){
+		document.getElementById('strict').style.background = '#eee';
+		hardcore = true;		
+	} else if(hardcore == true){
+		document.getElementById('strict').style.background = '#777';
+		hardcore = false;		
+	}
+
+}
+//AUDIOS 
+var audio1 = document.getElementById("audio1");
+var audio2 = document.getElementById("audio2");
+var audio3 = document.getElementById("audio3");
+var audio4 = document.getElementById("audio4");
+
 //playIt assigns numbers from our moves list to the function changeColor and plays the following steps depending on the Stage (j).
 function playIt(){
 	if(moves[p] == 0){
 		function changeColor0(){
 			setTimeout(function(){
 				document.getElementById('left-top').style.background = 'white';
+				audio1.play();
 				function backToOriginalColor0(){	
 					setTimeout(function(){
 					document.getElementById('left-top').style.background = '#333';
@@ -66,6 +85,7 @@ function playIt(){
 		function changeColor1(){
 			setTimeout(function(){
 				document.getElementById('right-top').style.background = 'white';
+				audio2.play();
 				function backToOriginalColor1(){	
 					setTimeout(function(){
 					document.getElementById('right-top').style.background = '#444';
@@ -90,6 +110,7 @@ function playIt(){
 		function changeColor2(){
 			setTimeout(function(){
 				document.getElementById('left-bottom').style.background = 'white';
+				audio3.play();
 				function backToOriginalColor2(){	
 					setTimeout(function(){
 					document.getElementById('left-bottom').style.background = '#555';
@@ -113,6 +134,7 @@ function playIt(){
 		function changeColor3(){
 			setTimeout(function(){
 				document.getElementById('right-bottom').style.background = 'white';
+				audio4.play();
 				function backToOriginalColor3(){	
 					setTimeout(function(){
 					document.getElementById('right-bottom').style.background = '#666';
@@ -140,6 +162,7 @@ var i = 0;
 //functions adding Player's moves to an answers array and calls verify function after each Click
 function playerMove0(){
 	if(playerTurn){
+		audio1.play();
 		answers.push(0);
 		verify();
 	}
@@ -149,6 +172,7 @@ function playerMove0(){
 
 function playerMove1(){
 	if(playerTurn){
+		audio2.play();
 		answers.push(1);
 		verify();
 	}
@@ -156,6 +180,7 @@ function playerMove1(){
 
 function playerMove2(){
 	if(playerTurn){
+		audio3.play();
 		answers.push(2);
 		verify();
 	}
@@ -164,6 +189,7 @@ function playerMove2(){
 
 function playerMove3(){
 	if(playerTurn){
+		audio4.play();
 		answers.push(3);
 		verify();
 	}
@@ -173,7 +199,7 @@ function playerMove3(){
 
 //Function verify checks after each step if it's matching MOVES array
 function verify(){
-	console.log(answers);
+
 	if(moves[i] == answers[i]){
 			i++;
 			ifFinished();
@@ -182,39 +208,74 @@ function verify(){
 		replay();
 
 	}
-	}
+}
+
 	//isFinished checks after each CLICK if this is the end of Player's turn
-	function ifFinished(){
-		if(i == j+1){
-			i=0;
-			playerTurn = false;
-			nextStage();
-		}
+function ifFinished(){
+	if(i == j+1 && j+1 < 20){
+		i=0;
+		playerTurn = false;
+		nextStage();
+	} else if(j+1 == 20){
+		victory();
+	}
 }
 
 function replay(){
-	answers = [];
-	p = 0;
-	i=0;
-	document.getElementById('status').style.height = "50px";
-	document.getElementById('status').innerHTML = "TRY AGAIN";
-	document.getElementById('left-top').className = document.getElementById('left-top').className + " fail";
-	document.getElementById('right-top').className = document.getElementById('right-top').className + " fail";
-	document.getElementById('left-bottom').className = document.getElementById('left-bottom').className + " fail";
-	document.getElementById('right-bottom').className = document.getElementById('right-bottom').className + " fail";
-	function goDark(){
-		setTimeout(function(){
-				document.getElementById('left-top').className = document.getElementById('left-top').className.replace("fail", '');
-				document.getElementById('right-top').className = document.getElementById('right-top').className.replace("fail", '');
-				document.getElementById('left-bottom').className = document.getElementById('left-bottom').className.replace("fail", '');
-				document.getElementById('right-bottom').className = document.getElementById('right-bottom').className.replace("fail", '');
-				document.getElementById('status').style.height = "0px";
-				document.getElementById('status').innerHTML = "";
-		}, 500);
-	}	
-	goDark();
+	if(hardcore == false){
+		answers = [];
+		p = 0;
+		i=0;
+		document.getElementById('status').style.height = "50px";
+		document.getElementById('status').innerHTML = "OUPS... TRY AGAIN...";
+		document.getElementById('left-top').className = document.getElementById('left-top').className + " fail";
+		document.getElementById('right-top').className = document.getElementById('right-top').className + " fail";
+		document.getElementById('left-bottom').className = document.getElementById('left-bottom').className + " fail";
+		document.getElementById('right-bottom').className = document.getElementById('right-bottom').className + " fail";
+		function goDark(){
+			setTimeout(function(){
+					document.getElementById('left-top').className = document.getElementById('left-top').className.replace("fail", '');
+					document.getElementById('right-top').className = document.getElementById('right-top').className.replace("fail", '');
+					document.getElementById('left-bottom').className = document.getElementById('left-bottom').className.replace("fail", '');
+					document.getElementById('right-bottom').className = document.getElementById('right-bottom').className.replace("fail", '');
+					document.getElementById('status').style.height = "0px";
+					document.getElementById('status').innerHTML = "";
+			}, 500);
+		}	
+		goDark();
+		playerTurn = false;
+		playIt();
+	}else if(hardcore == true){
+		answers = [];
+		p = 0;
+		i=0;
+		document.getElementById('status').style.height = "70px";
+		document.getElementById('status').innerHTML = "START AGAIN!";
+		document.getElementById('left-top').className = document.getElementById('left-top').className + " fail";
+		document.getElementById('right-top').className = document.getElementById('right-top').className + " fail";
+		document.getElementById('left-bottom').className = document.getElementById('left-bottom').className + " fail";
+		document.getElementById('right-bottom').className = document.getElementById('right-bottom').className + " fail";
+		function goDark(){
+			setTimeout(function(){
+					document.getElementById('left-top').className = document.getElementById('left-top').className.replace("fail", '');
+					document.getElementById('right-top').className = document.getElementById('right-top').className.replace("fail", '');
+					document.getElementById('left-bottom').className = document.getElementById('left-bottom').className.replace("fail", '');
+					document.getElementById('right-bottom').className = document.getElementById('right-bottom').className.replace("fail", '');
+					document.getElementById('status').style.height = "0px";
+					document.getElementById('status').innerHTML = "";
+					playerTurn = false;
+					gameStarted = false;
+					moves = [];
+					firstStart();
+			}, 1500);
+		}	
+		goDark();
+		playerTurn = false;
+		gameStarted = false;
+		moves = [];
+		firstStart();
 
-	playIt();
+	}
 }
 
 
@@ -223,6 +284,8 @@ function nextStage(){
 	document.getElementById('count').innerHTML = j+1;
 	answers = [];
 	p=0;
+	document.getElementById('status').style.height = "50px";
+	document.getElementById('status').innerHTML = "GOOD!";
 	document.getElementById('left-top').className = document.getElementById('left-top').className + " win";
 	document.getElementById('right-top').className = document.getElementById('right-top').className + " win";
 	document.getElementById('left-bottom').className = document.getElementById('left-bottom').className + " win";
@@ -233,16 +296,25 @@ function nextStage(){
 				document.getElementById('right-top').className = document.getElementById('right-top').className.replace("win", '');
 				document.getElementById('left-bottom').className = document.getElementById('left-bottom').className.replace("win", '');
 				document.getElementById('right-bottom').className = document.getElementById('right-bottom').className.replace("win", '');
+				document.getElementById('status').style.height = "0px";
+				document.getElementById('status').innerHTML = "";
 		}, 500);
 	}	
 	goBright();
+	playerTurn = false;
 	playIt();
 
+}
+
+function victory(){
+	document.getElementById('status').style.height = '50px';
+	document.getElementById('status').innerHTML = "CONGRATS! YOU'VE MADE IT!";
 }
 
 
 //clickers go to the bottom!
 document.getElementById('start').addEventListener('click', firstStart);
+document.getElementById('strict').addEventListener('click', hardcoreMode);
 
 document.getElementById('left-top').addEventListener('click', playerMove0);
 document.getElementById('right-top').addEventListener('click', playerMove1);
